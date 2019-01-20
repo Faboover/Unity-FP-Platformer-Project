a73d6bc7a20fc338fbab403cf08b5cd96f5c05ea
Faboover <fjlefevre@oakland.edu>
2019-01-19 22:54:32 -0500

    Walk, Turn Left/Right, Look Up/Down, and Partial Jump Implemented
    Also added input to project settings for both keyboard and mouse.
    Joy X/Y, Jump, Slide buttons added
    
    Will adjust as project continues forward.

---
+++ FP TF TT Platformer/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+
+    public float sensitivityY = 5F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    float rotationX = 0F;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Only Rotates head up and down
+        if (axes == RotationAxes.MouseXAndY)
+        {
+
+            rotationX = transform.localEulerAngles.x + -(Input.GetAxis("Mouse Y")) * sensitivityY;
+            //rotationX = Mathf.Clamp (rotationX, minimumY, maximumY);
+            if (rotationX <= 60.0f || rotationX >= 300.0f)
+            {
+                transform.localEulerAngles = new Vector3(rotationX, 90, 0);
+            }
+
+        }
+        //		else if (axes == RotationAxes.MouseX)
+        //		{
+        //			transform.Rotate(Input.GetAxis("Mouse X") * sensitivityX, 0, 0);
+        //		}
+        //		else
+        //		{
+        //			rotationX += Input.GetAxis("Mouse Y") * sensitivityY;
+        //			rotationX = Mathf.Clamp (rotationX, minimumY, maximumY);
+        //			
+        //			transform.localEulerAngles = new Vector3(-rotationX, transform.localEulerAngles.y, 0);
+        //		}
+
+    }
+}
--- FP TF TT Platformer/Assets/Scripts/PlayerMovement.cs
+++ FP TF TT Platformer/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,172 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 };
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+
+    public float sensitivityX = 5F;
+    public float joySensitivityX = 10F;
+
+    // Set the min and max of the rotation
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    // movement speed of the player
+    public float moveSpeed = 0.1f;
+    public float sprintSpeed = 0.3f;
+    public float speed;
+
+    // Used for vertical Rotation, turning left or right
+    float rotationY = 0.0f;
+
+    // Used for knowing when the player is back on a flat or tagged ground object
+    public bool onGround = false;
+
+    // Used to know when the player is sprinting
+    public bool isSprinting = false;
+
+    // Used for knowing whether the player is sliding or ready to slide
+    // Ready to slide is for when the player hits the slide button while in the air
+    public bool isSliding = false;
+    public bool readytoSlide = false;
+
+
+    // Vertical upward force to throw the player up when jumping
+    // Set in the Unity Inspecter
+    public Vector3 jumpForce;
+
+    // In Titanfall 2, jumping while sliding doesn't go as high as jumping when not sliding.
+    // Could use for double jump
+    // Set in the Unity Inspecter
+    public Vector3 slidejumpForce;
+
+    // This is used to know whether or not the game has been paused
+    public GameObject eventHandler;
+
+    // Use this for initialization
+    void Start ()
+    {
+        speed = moveSpeed;
+
+        // Event Handler not yet made, starting with basic moving for both controller
+        // and mouse and keyboard
+        //eventHandler = GameObject.FindGameObjectWithTag("Handler");
+    }
+
+    // All collision instances
+    void OnCollisionEnter(Collision obj)
+    {
+        if (obj.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+            
+            if (readytoSlide)
+            {
+                isSliding = true;
+            }
+        }
+    }
+
+    void OnCollisionStay(Collision obj)
+    {
+        if (obj.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+    }
+
+    void OnCollisionExit(Collision obj)
+    {
+        if (obj.gameObject.CompareTag("Ground"))
+        {
+            onGround = false;
+        }
+    }
+
+    // Jump function - will handle jump force for what the current player status is
+    // Will depend on whether the chararcter is in the air, on the ground, sliding, or 
+    // wall running
+    void Jump()
+    {
+        Debug.Log("Jump Function entered");
+        if (onGround && !isSliding)
+        {
+            Debug.Log("Normal Jump: OnGround !isSliding");
+            this.GetComponent<Rigidbody>().AddForce(jumpForce);
+        }
+        else if (onGround && isSliding)
+        {
+            Debug.Log("Slide Jump: OnGround isSliding");
+            this.GetComponent<Rigidbody>().AddForce(slidejumpForce);
+        }
+        else
+        {
+            Debug.Log("Else Jump:");
+            this.GetComponent<Rigidbody>().AddForce(jumpForce);
+        }
+    }
+
+    void Sprint()
+    {
+
+    }
+
+    void Slide()
+    {
+        if (onGround && isSprinting)
+        {
+            isSliding = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        // Moves body left, right, forward and back
+        float x_velocity = Input.GetAxisRaw("Horizontal") * speed;
+        float z_velocity = Input.GetAxisRaw("Vertical") * speed;
+
+        // Its best to look up for yourself what is going on from this point on
+        // This was code that I got by looking for a way to do a first person camera
+        transform.Translate(new Vector3(z_velocity, 0, -x_velocity));
+
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            //Debug.Log (Input.GetJoystickNames ().Length);
+
+            // If a controller is plugged in
+            if (Input.GetJoystickNames().Length != 0)
+            {
+                rotationY = transform.localEulerAngles.y + Input.GetAxis("JoyX") * joySensitivityX;
+            }
+            else
+            {
+                rotationY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+            }
+
+            transform.localEulerAngles = new Vector3(0, rotationY, 0);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Jump Button Pressed!!");
+            Jump();
+        }
+
+        if (Input.GetButtonDown("Slide"))
+        {
+            if (readytoSlide)
+            {
+                readytoSlide = false;
+            }
+            else
+            {
+                readytoSlide = true;
+                Slide();
+            }
+        }
+    }
 }
