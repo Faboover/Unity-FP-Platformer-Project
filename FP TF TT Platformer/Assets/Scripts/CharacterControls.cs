22cf01deaae592430e6fc30a71e280c63f8b32dc
Faboover <fjlefevre@oakland.edu>
2019-02-25 23:19:46 -0500

    Wall Jump Implemented
    Wall jump implemented through rigidbody. Had to adjust OnCollision functions to accomadate new way of detecting collisions through obj.contacts and each contacts' normal vector attribute. Able to detect if on ground or on wall through those normal vectors.

--- FP TF TT Platformer/Assets/Scripts/CharacterControls.cs
+++ FP TF TT Platformer/Assets/Scripts/CharacterControls.cs
@@ -12,6 +12,7 @@ public class CharacterControls : MonoBehaviour
     private Rigidbody rigid;
 
     private Vector2 xzVelocity;
+    private Vector2 wallNormal;
 
     private CharacterController controller;
 
@@ -31,10 +32,14 @@ public class CharacterControls : MonoBehaviour
     public float maxVelocityChange = 10.0f;
 
     public bool canJump = true;
+
     public bool onGround = false;
+    public bool onWall = false;
+
     public bool isCrouched = false;
     public bool isSprinting = false;
     public bool isSliding = false;
+    public bool isMoving = false;
 
     public float jumpHeight = 2.0f;
     
@@ -55,43 +60,104 @@ public class CharacterControls : MonoBehaviour
 
     void OnCollisionEnter(Collision obj)
     {
+        // Stops Rigidbody from being zeroed out for On Collision Enter
+        // Now able to Slide after landing on the ground
+        rigid.velocity = -obj.relativeVelocity;
+
+        // Goes through all contacts with rigidbody
+        foreach (ContactPoint contact in obj.contacts)
+        {
+            Debug.Log("CollisionEnter Detected: Contact Point " + contact.point + " Contact Normal " + contact.normal);
+
+            if (contact.normal.y > 0)
+            {
+                onGround = true;
+                canJump = true;
+
+                xzVelocity = new Vector2(rigid.velocity.x, rigid.velocity.z);
+
+                if (isCrouched && xzVelocity.magnitude <= 10)
+                {
+                    Debug.Log("Collision: CROUCHED, LANDED, AND MAG <= 10, speed = crouchSpeed");
+                    speed = crouchSpeed;
+                }
+            }
+            else if ((contact.normal.x != 0 || contact.normal.z != 0) && contact.normal.y == 0)
+            {
+                Debug.Log("CollisionEnter Detected Wall: Contact Normal " + contact.normal);
+                if (!onGround)
+                {
+                    onWall = true;
+                }
+
+                canJump = true;
+
+                wallNormal = new Vector2(contact.normal.x, contact.normal.z);
+            }
+        }
+
+        /*
         if (obj.gameObject.CompareTag("Ground"))
         {
             onGround = true;
             canJump = true;
 
-            // Stops Rigidbody from being zeroed out for On Collision Enter
-            // Now able to Slide after landing on the ground
-            rigid.velocity = -obj.relativeVelocity;
             xzVelocity = new Vector2(rigid.velocity.x, rigid.velocity.z);
             
-
             if (isCrouched && xzVelocity.magnitude <= 10)
             {
                 Debug.Log("CROUCHED, LANDED, AND MAG <= 10, speed = crouchSpeed");
                 speed = crouchSpeed;
             }
         }
+        */
     }
 
     void OnCollisionStay(Collision obj)
     {
-        if (obj.gameObject.CompareTag("Ground"))
+        // Goes through all contacts with rigidbody
+        foreach (ContactPoint contact in obj.contacts)
         {
-            onGround = true;
-            canJump = true;
+            Debug.Log("CollisionEnter Detected: Contact Point " + contact.point + " Contact Normal " + contact.normal);
+
+            if (contact.normal.y > 0)
+            {
+                onGround = true;
+                canJump = true;
+
+                xzVelocity = new Vector2(rigid.velocity.x, rigid.velocity.z);
+
+                if (isCrouched && xzVelocity.magnitude <= 10)
+                {
+                    Debug.Log("Collision: CROUCHED, LANDED, AND MAG <= 10, speed = crouchSpeed");
+                    speed = crouchSpeed;
+                }
+            }
+            else if ((contact.normal.x != 0 || contact.normal.z != 0) && contact.normal.y == 0)
+            {
+                Debug.Log("CollisionEnter Detected Wall: Contact Normal " + contact.normal);
+                if (!onGround)
+                {
+                    onWall = true;
+                }
+
+                canJump = true;
+
+                wallNormal = new Vector2(contact.normal.x, contact.normal.z);
+            }
         }
     }
 
     void OnCollisionExit(Collision obj)
     {
-        if (obj.gameObject.CompareTag("Ground"))
-        {
-            onGround = false;
-        }
+        Debug.Log("CollisionExit Detected: Contacts are - " + obj.contacts);
+
+        onGround = false;
+
+        onWall = false;
     }
 
-    private void OnControllerColliderHit(ControllerColliderHit hit)
+        private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         Debug.Log("Point hit - " + hit.point + "\nHit Normal - " + hit.normal);
     }
@@ -116,15 +182,24 @@ public class CharacterControls : MonoBehaviour
 
     void Jump()
     {
-        if (onGround)
+        if (!onWall)
         {
-            rigid.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
+            if (onGround)
+            {
+                rigid.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
+            }
+            else if (!onGround && canJump)
+            {
+                Debug.Log("Air Jump:");
+                canJump = false;
+                rigid.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
+            }
         }
-        else if (!onGround && canJump)
+        else
         {
-            Debug.Log("Air Jump:");
-            canJump = false;
-            rigid.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
+            Debug.Log("Wall Jump:");
+
+            rigid.velocity = new Vector3(wallNormal.x * speed, CalculateJumpVerticalSpeed(), wallNormal.y * speed);
         }
 
         //rigid.AddForce(0, CalculateJumpVerticalSpeed(), 0);
@@ -148,6 +223,41 @@ public class CharacterControls : MonoBehaviour
         rigid.AddForce(new Vector3(xzVelocity.x, 0, xzVelocity.y));
     }
 
+    void Move()
+    {
+
+        // Calculate how fast we should be moving
+        targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        targetVelocity = transform.TransformDirection(targetVelocity);
+        targetVelocity *= speed;
+
+        // Apply a force that attempts to reach our target velocity
+        velocity = rigid.velocity;
+        velocityChange = (targetVelocity - velocity);
+        velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+        velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+        velocityChange.y = 0;
+
+        rigid.AddForce(velocityChange, ForceMode.VelocityChange);
+    }
+
+    void AirMove()
+    {
+        // Calculate how fast we should be moving
+        targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        targetVelocity = transform.TransformDirection(targetVelocity);
+        targetVelocity *= speed;
+
+        // Apply a force that attempts to reach our target velocity
+        velocity = rigid.velocity;
+        velocityChange = (targetVelocity - velocity);
+        velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+        velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+        velocityChange.y = 0;
+
+        rigid.AddForce(velocityChange * 10);
+    }
+
     void FixedUpdate()
     {
         
@@ -163,7 +273,9 @@ public class CharacterControls : MonoBehaviour
             "\nVelocity Change: " + velocityChange +
             "\nCrrnt Velocity: " + rigid.velocity +
             "\nMagnitude: " + rigid.velocity.magnitude +
-            "\nXZMagnitude: " + xzVelocity.magnitude;
+            "\nXZMagnitude: " + xzVelocity.magnitude +
+            "\nHorizontal: " + Input.GetAxis("Horizontal") + 
+            "\nVertical: " + Input.GetAxis("Vertical");
 
         // Turning the Player left or right
         if (axes == RotationAxes.MouseXAndY)
@@ -183,27 +295,32 @@ public class CharacterControls : MonoBehaviour
             }
         }
 
-
-        // Calculate how fast we should be moving
-        targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        targetVelocity = transform.TransformDirection(targetVelocity);
-        targetVelocity *= speed;
-
-        // Apply a force that attempts to reach our target velocity
-        velocity = rigid.velocity;
-        velocityChange = (targetVelocity - velocity);
-        velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
-        velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
-        velocityChange.y = 0;
-
-        if (!isSliding)
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
         {
-            rigid.AddForce(velocityChange, ForceMode.VelocityChange);
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+
+        if (onGround)
+        {
+            if (!isSliding)
+            {
+                Move();
+            }
+        }
+        else if(!onGround && isMoving)
+        {
+            AirMove();
         }
 
         // Jump
         if (canJump && Input.GetButtonDown("Jump"))
         {
+            isCrouched = false;
             Jump();
         }
 
